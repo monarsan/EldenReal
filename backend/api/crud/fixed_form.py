816e984b2030021ffb1a11dd4ab5fd94cd37d5a5
monarsan <aaaaa333445@gmail.com>
2022-05-07 22:33:28 +0900

    merge comment, popup and fixed_form api

--- backend/api/crud/fixed_form.py
+++ backend/api/crud/fixed_form.py
@@ -1,1 +1,24 @@
 import models.fixed_form
+from fastapi import APIRouter, HTTPException, Depends, status
+from schemas.fixed_form import FixedFormCreate
+from crud.util import add_db
+from db import get_db
+
+router = APIRouter()
+
+@router.post("/api/fixed_form/create")
+def create_fixed_form(form: FixedFormCreate, db = Depends(get_db)):
+    fixed_form_obj = models.fixed_form.FixedForm(
+        content = form.content,
+        ok_good = form.ok_good
+    )
+    add_db(db, fixed_form_obj)
+    return fixed_form_obj
+
+
+@router.get("/api/fixed_form/{id}")
+def get_fixed_form_by_id(id:int, db = Depends(get_db)):
+    db_fixed_form_by_id = db.query(models.fixed_form.FixedForm).filter(models.fixed_form.FixedForm.id == id).first()
+    if db_fixed_form_by_id is None:
+        raise HTTPException(status_code = 404, detail = "fixed_form not found")
+    return db_fixed_form_by_id

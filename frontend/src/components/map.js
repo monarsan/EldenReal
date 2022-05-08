46d5ec27adc9a0cf8442680d5dc71cc1c6806d77
monarsan <aaaaa333445@gmail.com>
2022-05-08 10:16:05 +0900

    save map

--- frontend/src/components/map.js
+++ frontend/src/components/map.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { GoogleMap, LoadScript } from "@react-google-maps/api";
+// import displayComment from "./comments";
 
 const map_id = ["660c68bf4827712e"];
 const key = "AIzaSyBZTJ4yuVc6jdJFJoQ6ii3LfmU6Jz3WfQI";
 
 function Map(props) {
+  var map = new google.maps.Map(document.getElementById("map"), opts);
+  var latlng = map.getCenter();
+  var lat = latlng.lat();
+  var lng = latlng.lng();
   return (
     <LoadScript googleMapsApiKey={key}>
       <GoogleMap
@@ -12,7 +17,7 @@ function Map(props) {
         center={props.center}
         zoom={10}
         options={{ mapId: map_id[0] }}
-        onDragEnd={displayComment()}
+        onDragEnd = {()=> console.log(lat)}
       >
       </GoogleMap>
     </LoadScript>
